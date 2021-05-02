4a11061a65386f7ab6d84f32aecfdb3c60f3f94a
Joseph Moberly <jmobes15@gmail.com>
2021-05-01 20:22:19 -0700

    finished home page jsx and css styling

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.css";
 import SearchIcon from "@material-ui/icons/Search";
 import NearMeIcon from "@material-ui/icons/NearMe";
+import TextInput from "../../components/TextInput/TextInput";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <span className="home__title--1">Start Your Adventure</span>
         <span className="home__title--2">Let us help with ideas</span>
       </h2>
+      <TextInput />
     </section>
   );
 };
