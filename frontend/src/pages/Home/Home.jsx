6158ccc8e5659303c6323426966b63499bebd773
Joseph Moberly <jmobes15@gmail.com>
2021-05-01 19:40:02 -0700

    added a title using a google font.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,14 @@ import SearchIcon from "@material-ui/icons/Search";
 import NearMeIcon from "@material-ui/icons/NearMe";
 
 const Home = () => {
-  return <section className="home"></section>;
+  return (
+    <section className="home">
+      <h2 className="home__title">
+        <span className="home__title--1">Start Your Adventure</span>
+        <span className="home__title--2">Let us help with ideas</span>
+      </h2>
+    </section>
+  );
 };
 
 export default Home;
